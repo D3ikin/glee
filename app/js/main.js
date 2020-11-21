547284d8a0b64954d5a78f308321389721fe111c
d3ikin <deikincoin@gmail.com>
2020-11-21 14:36:23 +0200

    added mixitup and fancybox

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,15 @@ $(function(){
     // autoplay: true,
     // autoplaySpeed: 2500
   });
+
+  var mixerProducts = mixitup('.products__inner', {
+    selectors: {
+      control: '.products__filter-btn'
+    }
+  });
+  var mixerDesign = mixitup('.design__wrapper', {
+    selectors: {
+      control: '.design__filter-btn'
+    }
+  });
 });
